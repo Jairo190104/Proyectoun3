15927c535b4e0e236ec2b10e8a01d7fc31b6b8ef
Jairo190104 <84990054+Jairo190104@users.noreply.github.com>
2021-05-31 11:31:49 -0500

    Add files via upload

---
+++ formu.js
@@ -0,0 +1,18 @@
+function limpiarFormulario(){
+	document.getElementById("miForm").reset();
+}
+
+var ac=function(n1, n2, n3){ 
+	var vf = parseInt(document.getElementById("vf").value);
+	var vi = parseInt(document.getElementById("vi").value);
+	var t = parseInt(document.getElementById("t").value);
+    aceleracion = (vf-vi)/t;
+    return "La aceleracion es igual a " + aceleracion + " m/s^2";
+}
+
+var di=function(n1){
+	var a = parseInt(document.getElementById("nm").value);
+	var exp = parseInt(document.getElementById("exponente").value);
+	resultado = (a*exp);
+	return "Es igual a " + resultado + "e^" + exp;  
+}
